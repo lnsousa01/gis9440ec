cba57793c6ce802bb701797e2abf6df5fce0944f
Lauren Sousa <lauren.sousa@baruchmail.cuny.edu>
2024-12-06 20:46:03 +0000

    commit

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,12 +1,12 @@
 WITH 
 orders AS(
     select order_id, customer_id
-    FROM dbt-extra-credit.dbt_lsousa2.stg_jaffle_shop__orders
+    FROM dbt-extra-credit.extracreditproject.stg_jaffle_shop__orders
 ),
 amount AS(
-    SELECT amount
-    FROM dbt-extra-credit.dbt_lsousa2.stg_stripe__payments
+    SELECT amount, order_id
+    FROM dbt-extra-credit.extracreditproject.stg_stripe__payments
 ),
 final AS(
-    SELECT * FROM orders LEFT JOIN amount ON order_id)
+    SELECT * FROM orders LEFT JOIN amount USING (order_id))
 SELECT * FROM final
