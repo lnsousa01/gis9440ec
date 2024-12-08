7784f4710670e68d0a46779778c77fee334b6112
Lauren Sousa <lauren.sousa@baruchmail.cuny.edu>
2024-12-08 01:34:52 +0000

    commit

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,7 +3,6 @@ id as payment_id,
 orderid as order_id,
 paymentmethod as payment_method,
 status,
---below two changes to match Exemplar model:
 amount/100 as amount
 created as created_at
 from {{source('stripe', 'payment')}}
